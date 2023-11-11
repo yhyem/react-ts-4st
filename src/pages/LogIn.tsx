26925c7e4eb49928e47a4b30ed8bd2845e10dc8c
ndaemy <yuyaebean@gmail.com>
2023-11-11 17:03:52 +0900

    feat: react-hook-form 으로 form 관리하기

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,8 +1,34 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
+
 export const LogIn = () => {
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = data => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <h1>Log In</h1>
-      <p>This is the log in page.</p>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        style={{ display: "flex", flexDirection: "column", gap: 24, maxWidth: 512, margin: 12 }}
+      >
+        <div style={{ display: "flex", flexDirection: "column", gap: 6 }}>
+          <input type="email" placeholder="Email" {...register("email", { required: true })} />
+          <input
+            type="password"
+            placeholder="Password"
+            {...register("password", { required: true })}
+          />
+        </div>
+
+        <input type="submit" />
+      </form>
     </div>
   );
 };
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,8 +1,36 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+  username: string;
+};
+
 export const SignUp = () => {
+  const { register, handleSubmit } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = data => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <h1>Sign Up</h1>
-      <p>This is the sign up page.</p>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        style={{ display: "flex", flexDirection: "column", gap: 24, maxWidth: 512, margin: 12 }}
+      >
+        <div style={{ display: "flex", flexDirection: "column", gap: 6 }}>
+          <input type="email" placeholder="Email" {...register("email", { required: true })} />
+          <input
+            type="password"
+            placeholder="Password"
+            {...register("password", { required: true })}
+          />
+          <input type="text" placeholder="Username" {...register("username", { required: true })} />
+        </div>
+
+        <input type="submit" />
+      </form>
     </div>
   );
 };
