28083208063b99c6bd63ec7e940dbf53935987a9
yhyem <yoonhyemin0120@gmail.com>
2023-11-21 23:02:07 +0900

    feat: Header 컴포넌트 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,44 @@
 import { Link } from "react-router-dom";
+import Lion from "../assets/Lion.svg";
+import { styled } from "styled-components";
+import Tab from "./Tab";
 
 export const Header = () => {
   return (
-    <div style={{ display: "flex", gap: 12 }}>
-      <Link to="/">User List</Link>
-      <Link to="/sign-up">Sign Up</Link>
-      <Link to="/log-in">Log In</Link>
-    </div>
+    <HeaderWrapper>
+      <ContenetWrapper>
+        <Link to="/">
+          <img src={Lion} alt="logo_lion" />
+        </Link>
+        <ButtonWrapper>
+          <Tab link="/log-in" text="로그인" />
+          <Tab link="/sign-up" text="회원가입" />
+        </ButtonWrapper>
+      </ContenetWrapper>
+    </HeaderWrapper>
   );
 };
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  height: 70px;
+  min-width: 320px;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ContenetWrapper = styled.div`
+  display: flex;
+  padding: 0px 20px;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 40px;
+  justify-content: flex-end;
+  align-items: center;
+`;
