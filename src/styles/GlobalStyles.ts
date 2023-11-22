5acbbe2c6965d9aad4b3bb00d27179836be1055c
yhyem <yoonhyemin0120@gmail.com>
2023-11-22 23:37:21 +0900

    setting: GlobalStyles 기본 css 설정

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    *{
+        box-sizing: border-box;
+    }
+    html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
+    a, dl, dt, dd, ol, ul, li, form, label, table{
+        margin: 0;
+        padding: 0;
+        border: 0;
+    }
+    ol, ul{
+        list-style: none;
+    }
+    button {
+        border: 0;
+        background: transparent;
+        cursor: pointer;
+    }
+`;
+
+export default GlobalStyles;
