c37b08ceb02052e809986f1de018fb1e65fd0b47
yhyem <yoonhyemin0120@gmail.com>
2023-11-22 23:40:06 +0900

    feat: Signup 페이지 구현

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,4 +1,7 @@
 import { SubmitHandler, useForm } from "react-hook-form";
+import Input from "../components/Input";
+import { styled } from "styled-components";
+import Button from "../components/Button";
 
 type Inputs = {
   email: string;
@@ -14,23 +17,64 @@ export const SignUp = () => {
   };
 
   return (
-    <div>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        style={{ display: "flex", flexDirection: "column", gap: 24, maxWidth: 512, margin: 12 }}
-      >
-        <div style={{ display: "flex", flexDirection: "column", gap: 6 }}>
-          <input type="email" placeholder="Email" {...register("email", { required: true })} />
-          <input
+    <LoginWrapper>
+      <FormBlock onSubmit={handleSubmit(onSubmit)}>
+        <Title>회원가입</Title>
+        <LoginBlock>
+          <Input
+            title="이름"
+            type="email"
+            placeholder="Email"
+            {...register("email", { required: true })}
+          />
+          <Input
+            title="이메일"
             type="password"
             placeholder="Password"
             {...register("password", { required: true })}
           />
-          <input type="text" placeholder="Username" {...register("username", { required: true })} />
-        </div>
+          <Input
+            title="비밀번호"
+            type="text"
+            placeholder="Username"
+            {...register("username", { required: true })}
+          />
+        </LoginBlock>
 
-        <input type="submit" />
-      </form>
-    </div>
+        <Button type="submit" text="가입하기" />
+      </FormBlock>
+    </LoginWrapper>
   );
 };
+
+const LoginWrapper = styled.div`
+  margin-top: 100px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const LoginBlock = styled.div`
+  display: flex;
+  min-width: 320px;
+  max-width: 500px;
+  flex-direction: column;
+  gap: 26px;
+  align-self: stretch;
+`;
+
+const FormBlock = styled.form`
+  display: flex;
+  width: 500px;
+  min-width: 320px;
+  max-width: 500px;
+  flex-direction: column;
+  align-items: center;
+  gap: 70px;
+`;
+
+const Title = styled.div`
+  ${({ theme }) => theme.Typography.title1};
+  color: ${({ theme }) => theme.Color.GRAY1};
+  text-align: center;
+`;
